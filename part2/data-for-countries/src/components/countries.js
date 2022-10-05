9fe23bb00ba4a5a3f1b0ebbdbc5d0307aeef8449
Sarang Wadode <sarangwadode@gmail.com>
2022-10-05 11:18:43 +0530

    2.14: data fetched from weather api

--- part2/data-for-countries/src/components/countries.js
+++ part2/data-for-countries/src/components/countries.js
@@ -3,25 +3,15 @@ import Country from "./country";
 
 const Countries = ({ country }) => {
   const [show, setShow] = useState(false);
-
   const handleClick = () => {
-    console.log("button is clicked");
     setShow(!show);
   };
 
   return (
     <div>
-      <>
-        {!show ? (
-            <>
-            {country.name.common}
-          </>
-        ) : null}
-      </>
-        <button onClick={handleClick}>
-            {show ? "hide" : "show"}
-        </button>
-        {show ? <Country country={country} /> : null}
+      {country.name.common}
+      <button onClick={handleClick}>{show ? "hide" : "show"}</button>
+      {show ? <Country country={country} /> : null}
     </div>
   );
 };
--- part2/data-for-countries/src/components/country.js
+++ part2/data-for-countries/src/components/country.js
@@ -1,21 +1,60 @@
-const Country = ({country}) => {
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY);
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`
+      )
+      .then((res) => {
+        console.log(res.data.current);
+        setWeather(res.data.current);
+      })
+      .catch((e) => console.log(e));
+  }, [country.capital]);
+
   return (
     <div>
-      <h2>{country.name.common}</h2>
       <div>
-        <p>capital {country.capital}</p>
-        <p>area {country.area}</p>
-      </div>
-      <div>
-        <b>languages</b>
-        <ul>
-          {Object.values(country.languages).map((lang, i) => (
-            <li key={i}>{lang}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <img width='240px' height='150px' src={country.flags.png} alt="flag" />
+        <h2>{country.name.common}</h2>
+        <div>
+          <p>capital {country.capital}</p>
+          <p>area {country.area}</p>
+        </div>
+        <div>
+          <b>languages</b>
+          <ul>
+            {Object.values(country.languages).map((lang, i) => (
+              <li key={i}>{lang}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <img
+            width="240px"
+            height="150px"
+            src={country.flags.png}
+            alt="flag"
+          />
+        </div>
+        <div>
+          <h2>Weather in {country.name.common}</h2>
+          <div>
+            <p>temperature: {weather.temperature} Celsius</p>
+            <p>weather descriptions: {weather.weather_descriptions}</p>
+            <img
+              width="160px"
+              height="100px"
+              src={weather.weather_icons}
+              alt="weather icon"
+            />
+            <p>wind speed: {weather.wind_speed} m/s</p>
+          </div>
+        </div>
       </div>
     </div>
   );
