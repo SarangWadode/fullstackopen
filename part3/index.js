5be405a865d4f5730d054b934f92cc2ff60c0553
Sarang Wadode <sarangwadode@gmail.com>
2022-10-09 12:21:21 +0530

    3.15-3.18: delete, update and error handling added

--- part3/index.js
+++ part3/index.js
@@ -17,16 +17,29 @@ app.use(
   morgan(":method :url :status :res[content-length] :response-time ms :person")
 );
 
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  
+  next(err)
+}
+
+app.use(errorHandler)
+
 // fetch all persons
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
   Person.find({})
     .then(person => {
       res.json(person)
     })
+    .catch(err => next(err))
 });
 
 //add person
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   const newPerson = new Person({
@@ -34,9 +47,11 @@ app.post("/api/persons", (req, res) => {
     number: body.number,
   });
   
-  newPerson.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  newPerson.save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch(err => next(err))
 });
 
 // get info of db
@@ -53,12 +68,38 @@ app.get("/info", (req, res) => {
 });
 
 // fetch by id
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       res.json(person)
     })
+    .catch(err => next(err))
 });
+
+// find by id and delete
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
+})
+
+//find by id and update
+app.put('/api/persons/:id', (req, res, next) => {
+  body = req.body
+  console.log(body)
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPersons => {
+      res.json(updatedPersons)
+    })
+    .catch(err => next(err))
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
